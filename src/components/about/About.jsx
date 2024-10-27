5b4d77f36030960f6b160d2465997dde7bf931e9
jeeva <jeelion22@gmail.com>
2024-10-27 21:12:03 +0530

    altered and added content to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,8 +6,24 @@ const About = () => {
     <section className="container-fluid  section" id="about">
       <div className="row">
         <div className="col introduction">
-          <p>Hi there!,</p>
-          <p>I am Jeeva Madhaiyan, web developer specialized in MERN Stack</p>
+          <h2 className="title">MERN DEVELOPER</h2>
+          <h3 className="greet">
+            Hello, <br /> My name is Jeeva Madhaiyan
+          </h3>
+          <p>
+            I'm a full-stack developer with a passion for building scalable and
+            responsive web applications.
+          </p>
+          <p>
+            I have experience with front-end tools like HTML, CSS, JavaScript,
+            and React, along with back-end expertise in Node.js, Express, and
+            MongoDB.
+          </p>
+          <p>
+            I enjoy learning about new tech trends and am excited to bring my
+            skills to projects in dynamic, collaborative environments and
+            real-world problems.
+          </p>
         </div>
         <div className="col"></div>
       </div>
