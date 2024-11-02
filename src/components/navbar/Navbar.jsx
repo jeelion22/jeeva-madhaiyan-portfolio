8c188155ac87fef2831c89e040388d676516816f
jeeva <jeelion22@gmail.com>
2024-11-02 10:13:20 +0530

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <nav className="navbar navbar-expand-lg  fixed-top">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           {`C</>DEv.`}
