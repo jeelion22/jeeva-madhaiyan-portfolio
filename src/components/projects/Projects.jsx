9b5d51a13df4e8c245a670d92fadbbffe470aa55
jeeva <jeelion22@gmail.com>
2024-11-03 19:31:17 +0530

    additional projects added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,30 +1,57 @@
 import React from "react";
 import "./Projects.css";
-import travelPlannerImg from "./travel-planner.png";
-import reunitemeImg from "./reuniteme.png";
+import browserCode from "./icons/Browser-Code.png";
+import serverCode from "./icons/Database-Server.png";
+import web from "./icons/Web.png";
+
+import { projectsInfo } from "./projectData";
 
 const Project = ({ project }) => {
   return (
-    <div class="card h-100" style={{}}>
-      <div className="img">
-        <img
-          src={project.img}
-          class="card-img-top"
-          alt={project.name || "Project image"}
-        />
-      </div>
-
-      <div class="card-body">
-        <h5 class="card-title">{project.name}</h5>
-        <p class="card-text">{project.purpose}</p>
-
-        <div className="links">
-          <div className="source">
-            <a href=""></a>
-            <a href=""></a>
-          </div>
-          <div className="live">
-            <a href=""></a>
+    <div className="col">
+      <div className="card h-100">
+        <div className="img">
+          <img
+            src={project.img}
+            className="card-img-top"
+            alt={project.name || "Project image"}
+          />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{project.name}</h5>
+          <p className="card-text">{project.purpose}</p>
+        </div>
+        <div className="card-footer">
+          <div className="links">
+            <div className="source">
+              {project.links.client && (
+                <a
+                  href={project.links.client}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={browserCode} alt="Client" />
+                </a>
+              )}
+              {project.links.server && (
+                <a
+                  href={project.links.server}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={serverCode} alt="Server" />
+                </a>
+              )}
+              {project.links.live && (
+                <a
+                  href={project.links.live}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={web} alt="live link" />
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -32,64 +59,24 @@ const Project = ({ project }) => {
   );
 };
 
-const projectsInfo = [
-  {
-    name: "ReUniteME",
-    img: reunitemeImg,
-    purpose:
-      "A platform connecting mentally ill and missing individuals with their families.",
-    technologies: [
-      "ReactJS",
-      "Axios",
-      "NodeJS",
-      "ExpressJS",
-      "MongoDB",
-      "JWS",
-      "Mongoose",
-      "AWS S3",
-    ],
-  },
-  {
-    name: "Travel Planner India",
-    img: travelPlannerImg,
-    purpose:
-      "An Indian travel planning tool with itinerary and budget management.",
-    technologies: [
-      "ReactJS",
-      "Redux",
-      "NodeJS",
-      "ExpressJS",
-      "MongoDB",
-      "JWS",
-      "Mongoose",
-    ],
-  },
-];
-
 const Projects = () => {
   return (
     <section className="section" id="projects">
       <div className="container container-xxl">
-        <div className="row ">
+        <div className="row">
           <div className="col-md-12 showcase-description">
             <h2 className="project-header">Showcases.</h2>
-
             <p>
-              These are the some highlight projects. <br /> Each page discusses
-              the purpose of the project, what I learnt, and how I came up with
-              solution.{" "}
+              These are some highlight projects. <br /> Each page discusses the
+              purpose of the project, what I learned, and how I came up with
+              solutions.
             </p>
           </div>
         </div>
-
-        <div className="row">
-          <div className="col-md-12 col-sm-12">
-            <div className="project-container">
-              {projectsInfo.map((project, index) => (
-                <Project project={project} key={index} />
-              ))}
-            </div>
-          </div>
+        <div className="row row-cols-1 row-cols-md-3 g-4 project-container">
+          {projectsInfo.map((project, index) => (
+            <Project project={project} key={index} />
+          ))}
         </div>
       </div>
     </section>
