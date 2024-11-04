1dc08a9862c26f1dba443767506a462604319990
jeeva <jeelion22@gmail.com>
2024-11-04 13:03:58 +0530

    added downloadResume fn

---
+++ src/components/about/download_resume.js
@@ -0,0 +1,45 @@
+export const downloadResume = async (resumeUrl) => {
+  try {
+    const fileIdMatch = resumeUrl.match(/\/d\/(.*?)\//);
+
+    if (!fileIdMatch) {
+      throw new Error("Invalid Google DRIVE URL");
+    }
+
+    const fileId = fileIdMatch[1];
+
+    const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+    // const response = await fetch(downloadUrl, {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/pdf",
+    //   },
+    // });
+
+    // if (!response.ok) {
+    //   throw new Error("Failed to fetch resume file");
+    // }
+
+    // const blob = await response.blob();
+
+    // const url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+
+    link.href = downloadUrl;
+
+    link.download = "Jeeva_Madhaiyan_Resume.pdf";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    link.remove();
+
+    // window.URL.revokeObjectURL(url);
+  } catch (error) {
+    alert("Download failed!");
+    console.log(error);
+  }
+};
