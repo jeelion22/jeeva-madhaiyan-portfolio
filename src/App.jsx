cdbbf726f8a6961b57a7c1d759c7bbdf0d11bb6d
jeeva <jeelion22@gmail.com>
2025-06-10 18:14:25 +0530

    removed unsued components import

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/js/dist/collapse";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import About from "./components/about/About";
-import Projects from "./components/projects/Projects";
-import Skills from "./components/skills/Skills";
-import Education from "./components/education/Education";
-import Contact from "./components/contact/Contact";
-import Certification from "./components/certification/Certification";
+
 import Home from "./components/home/Home";
 
 const App = () => {
